0a32ad6a72f03e0684d5136acf277c8fb1baf05d
Ankit <ankit.1410025@kiet.edu>
2018-05-07 10:55:54 +0530

    staging Menu Ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,13 @@ import {ReportsModule} from './reports/reports.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule } from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import {MatProgressBarModule,MatSnackBarModule,MatRippleModule,MatTabsModule,MatCardModule,MatTableModule,MatPaginatorModule} from '@angular/material';
+import {MatProgressBarModule,MatSnackBarModule,MatRippleModule,MatTabsModule,MatInputModule,MatCardModule,MatTableModule,MatPaginatorModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {CdkTableModule} from '@angular/cdk/table';
 import { StorageServiceModule} from 'angular-webstorage-service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -30,7 +30,9 @@ import { StorageServiceModule} from 'angular-webstorage-service';
     FacilityInfoModule,
     ReportsModule,
     FormsModule,
+    MatInputModule,
     HttpModule,
+    ReactiveFormsModule,
     MatSnackBarModule,
     MatProgressBarModule,
     AppRoutingModule,
--- src/app/view-menu/view-menu.module.ts
+++ src/app/view-menu/view-menu.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuEditComponent } from './menu-edit/menu-edit.component';
 import {ViewMenuRoutingModule} from './view-menu-routing.module';
-
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material';
 @NgModule({
   declarations: [MenuEditComponent],
   imports: [
     CommonModule,
-    ViewMenuRoutingModule
+    ViewMenuRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   exports:[MenuEditComponent]
 })
