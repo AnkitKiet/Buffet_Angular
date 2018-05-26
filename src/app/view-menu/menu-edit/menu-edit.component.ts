3d4138273fe98ccc12d4a1c5a7044ae0bb7feaae
Ankit Maurya <ankit.1410025@kiet.edu>
2018-05-26 20:28:35 +0530

    Delete menu Added

--- src/app/view-menu/menu-edit/menu-edit.component.ts
+++ src/app/view-menu/menu-edit/menu-edit.component.ts
@@ -4,8 +4,9 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { Observable } from 'rxjs/Rx';
 import { category } from './categoryInterface';
-import {MatSnackBar} from '@angular/material';
-import {responseInterface} from './resInterface';
+import { MatSnackBar } from '@angular/material';
+import { responseInterface } from './resInterface';
+import { menu } from '../../facility-info/facility/menu_data_interface';
 
 @Component({
   selector: 'app-menu-edit',
@@ -14,21 +15,31 @@ import {responseInterface} from './resInterface';
 })
 export class MenuEditComponent implements OnInit {
 
+  private apiurlMenu = 'http://139.59.5.188:8081/getallmenu';
   private apiurl = 'http://139.59.5.188:8081/getallcategory';
   private submitapiurl = 'http://139.59.5.188:8081/addcategory';
-  private submitmenuurl='http://139.59.5.188:8081/addmenu'
+  private submitmenuurl = 'http://139.59.5.188:8081/addmenu'
+  private apiurlDelete = 'http://139.59.5.188:8081/deletemenu';
   data: any = {};
-  categorySelect:string;
+  categorySelect: string;
   public category: category;
-  public resInterface:responseInterface;
+  public resInterface: responseInterface;
   ELEMENT_DATA: category[];
   public postCategorydata: any;
-  public postMenuData:any;
-  constructor(private http: Http,public snackBar: MatSnackBar) {
+  public postMenuData: any;
+  dataMenu : any;
+  public menu : menu;
+  ELEMENT_DATA_MENU: menu[];
+  
+
+  constructor(private http: Http, public snackBar: MatSnackBar) {
     this.getMenu();
-    this.postCategorydata = {category:'',subCategory:''};
-    this.postMenuData = {category_id:'',menu_name:'',price:'',size:'',offer_id:'',toppings_allowed:''}
-   }
+    this.getAllMenu();
+    this.postCategorydata = { category: '', subCategory: '' };
+    this.postMenuData = { category_id: '', menu_name: '', price: '', size: '', offer_id: '', toppings_allowed: '' }
+    this.menu={id:'',menu_name:'',category_name:'',sub_category_name:'',price:'',size:'',offer_id:'',topping_count:'',image:'',category_id:'', cat_id:'',topping_id:'', topping_name:'',offer_details:'',discount_price:''};
+    this.dataMenu={menu_id:''};
+  }
 
   ngOnInit() {
   }
@@ -37,83 +48,116 @@ export class MenuEditComponent implements OnInit {
     return this.http.get(this.apiurl).map((res: Response) => res.json());
   }
 
+getDataMenu(){
+  return this.http.get(this.apiurlMenu).map((res: Response) => res.json());
+}
 
+getAllMenu(){
+  this.getDataMenu().subscribe(data => {
+ const ELEMENT_DATA_temp:menu[] =[...data.message.Sides, ...data.message.Pizza];
+  this.ELEMENT_DATA_MENU= ELEMENT_DATA_temp;
+  console.log(this.ELEMENT_DATA_MENU);
+  })
+}
 
   getMenu() {
     this.getData().subscribe(data => {
       const ELEMENT_DATA_temp: category[] = data.message;
       this.ELEMENT_DATA = ELEMENT_DATA_temp;
       console.log(this.ELEMENT_DATA);
-      
+
     })
   }
 
-getSelectedCategory(event:any){    
-  console.log(event.value);
-}
-
-submitCategoryData(body: Object): Observable<responseInterface> {
-        let bodyString = JSON.stringify(body); // Stringify payload
-         let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
-         let options = new RequestOptions({ headers: headers }); // Create a request option
-         return this.http.post(this.submitapiurl, body, options) // ...using post request
-                          .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
-                          .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+  //getting change event
+  getSelectedCategory(event: any) {
+    console.log(event.value);
   }
-  
 
-onSubmitCategory(){
-  this.submitCategoryData(this.postCategorydata).subscribe(data=>{
-    this.data=data;  
-    if(this.data.error=="false"){
-      this.openSnackBar("Success","");
-      this.getMenu();
-      this.postCategorydata.category='';
-      this.postCategorydata.subCategory='';
-    }else{
-      this.openSnackBar(this.data.message,"Retry");
-    }
-  });
-}
-
-submitMenuData(body: Object): Observable<responseInterface> {
-  let bodyString = JSON.stringify(body); // Stringify payload
-   let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
-   let options = new RequestOptions({ headers: headers }); // Create a request option
-   return this.http.post(this.submitmenuurl, body, options) // ...using post request
-                    .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
-                    .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
-}
-onSubmitMenu(){
-console.log(this.postMenuData);
-  this.submitMenuData(this.postMenuData).subscribe(data=>{
-    this.data=data;  
-    console.log(this.data);
-    if(this.data.error=="false"){
-      this.openSnackBar("Success","");
-      this.getMenu();
-      this.postMenuData.category_id='';
-      this.postMenuData.menu_name='';
-      this.postMenuData.price='';
-      this.postMenuData.size='';
-      this.postMenuData.offer_id='';
-      this.postMenuData.toppings_allowed='';
-
-    }else{
-      this.openSnackBar(this.data.message,"Retry");
-    }
-  });
-}
+  submitCategoryData(body: Object): Observable<responseInterface> {
+    let bodyString = JSON.stringify(body); // Stringify payload
+    let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+    let options = new RequestOptions({ headers: headers }); // Create a request option
+    return this.http.post(this.submitapiurl, body, options) // ...using post request
+      .map((res: Response) => res.json()) // ...and calling .json() on the response to return data
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+  }
 
 
-openSnackBar(message: string,action:string) {
-  let snackBarRef=this.snackBar.open(message,action,{
-    duration: 2000,
-  });
-  snackBarRef.onAction().subscribe(() => {
-    this.onSubmitCategory();
-});
-}
+  onSubmitCategory() {
+    this.submitCategoryData(this.postCategorydata).subscribe(data => {
+      this.data = data;
+      if (this.data.error == "false") {
+        this.openSnackBar("Success", "");
+        this.getMenu();
+        this.postCategorydata.category = '';
+        this.postCategorydata.subCategory = '';
+      } else {
+        this.openSnackBar(this.data.message, "Retry");
+      }
+    });
+  }
+
+  submitMenuData(body: Object): Observable<responseInterface> {
+    let bodyString = JSON.stringify(body); // Stringify payload
+    let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+    let options = new RequestOptions({ headers: headers }); // Create a request option
+    return this.http.post(this.submitmenuurl, body, options) // ...using post request
+      .map((res: Response) => res.json()) // ...and calling .json() on the response to return data
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+  }
+  onSubmitMenu() {
+    console.log(this.postMenuData);
+    this.submitMenuData(this.postMenuData).subscribe(data => {
+      this.data = data;
+      console.log(this.data);
+      if (this.data.error == "false") {
+        this.openSnackBar("Success", "");
+        this.getMenu();
+        this.postMenuData.category_id = '';
+        this.postMenuData.menu_name = '';
+        this.postMenuData.price = '';
+        this.postMenuData.size = '';
+        this.postMenuData.offer_id = '';
+        this.postMenuData.toppings_allowed = '';
+
+      } else {
+        this.openSnackBar(this.data.message, "Retry");
+      }
+    });
+  }
+
+  deleteMenuData(body: Object): Observable<responseInterface> {
+    let bodyString = JSON.stringify(body); // Stringify payload
+    let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+    let options = new RequestOptions({ headers: headers }); // Create a request option
+    return this.http.post(this.apiurlDelete, body, options) // ...using post request
+      .map((res: Response) => res.json()) // ...and calling .json() on the response to return data
+      .catch((error: any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+  }
+
+  onDelete(){
+    this.deleteMenuData(this.dataMenu).subscribe(data => {
+      this.data = data;
+      console.log(this.data);
+      if (this.data.error == "false") {
+        this.openSnackBar("Success", "");
+        this.getAllMenu();
+        this.dataMenu.menu_id = '';
+      } else {
+        this.openSnackBar(this.data.message, "Retry");
+      }
+    });
+  }
+
+  openSnackBar(message: string, action: string) {
+    let snackBarRef = this.snackBar.open(message, action, {
+      duration: 2000,
+    });
+    snackBarRef.onAction().subscribe(() => {
+      this.onSubmitCategory();
+    });
+  }
 
   Sizes = [
     { value: 'Regular', viewValue: 'Regular' },
